7a0080acb3fb27e748f33c63c69308046f9f115a
Ewelti <mischiefmao@gmail.com>
2024-09-12 16:17:23 -0600

    Making small improvements to abund and richness plots

--- R/Abund_Rich_plots/abund_rich_plots.R
+++ R/Abund_Rich_plots/abund_rich_plots.R
@@ -18,9 +18,11 @@ points(ests$abund_est[ests$trt=="bison"] ~ ests$mo_jit[ests$trt=="bison"],pch=21
 points(ests$abund_est[ests$trt=="bison"] ~ ests$mo_jit[ests$trt=="bison"], col="sienna",type="l",lwd=2)
 arrows(ests$mo_jit[ests$trt=="bison"], ests$abund_est[ests$trt=="bison"]-ests$abund_SE[ests$trt=="bison"], ests$mo_jit[ests$trt=="bison"], ests$abund_est[ests$trt=="bison"]+ests$abund_SE[ests$trt=="bison"],col="sienna",lwd=2,length=0.05, angle=90, code=3)
 ##cattle
-points(ests$abund_est[ests$trt=="cattle"] ~ ests$mo_jit[ests$trt=="cattle"],pch=21,col="gray0",bg="gray0",cex=2)
+points(ests$abund_est[ests$trt=="cattle"] ~ ests$mo_jit[ests$trt=="cattle"],pch=22,col="gray0",bg="gray0",cex=2)
 points(ests$abund_est[ests$trt=="cattle"] ~ ests$mo_jit[ests$trt=="cattle"],col="gray0",type="l",lwd=2)
 arrows(ests$mo_jit[ests$trt=="cattle"], ests$abund_est[ests$trt=="cattle"]-ests$abund_SE[ests$trt=="cattle"], ests$mo_jit[ests$trt=="cattle"], ests$abund_est[ests$trt=="cattle"]+ests$abund_SE[ests$trt=="cattle"],col="gray0",lwd=2,length=0.05, angle=90, code=3)
+##
+legend("topright",legend=c("A"), bty="n", cex=2)
 ##
 
 plot(1, type="n", xlim=c(5.7,8.3), ylim=c(1,6.5),las=1,ylab="",xlab="",xaxt="n")
@@ -33,11 +35,11 @@ points(ests$rich_est[ests$trt=="bison"] ~ ests$mo_jit[ests$trt=="bison"],pch=21,
 points(ests$rich_est[ests$trt=="bison"] ~ ests$mo_jit[ests$trt=="bison"],col="sienna",type="l",lwd=2)
 arrows(ests$mo_jit[ests$trt=="bison"], ests$rich_est[ests$trt=="bison"]-ests$rich_SE[ests$trt=="bison"], ests$mo_jit[ests$trt=="bison"], ests$rich_est[ests$trt=="bison"]+ests$rich_SE[ests$trt=="bison"],col="sienna",lwd=2,length=0.05, angle=90, code=3)
 ##cattle
-points(ests$rich_est[ests$trt=="cattle"] ~ ests$mo_jit[ests$trt=="cattle"],pch=21,col="gray0",bg="gray0",cex=2)
+points(ests$rich_est[ests$trt=="cattle"] ~ ests$mo_jit[ests$trt=="cattle"],pch=22,col="gray0",bg="gray0",cex=2)
 points(ests$rich_est[ests$trt=="cattle"] ~ ests$mo_jit[ests$trt=="cattle"],col="gray0",type="l",lwd=2)
 arrows(ests$mo_jit[ests$trt=="cattle"], ests$rich_est[ests$trt=="cattle"]-ests$rich_SE[ests$trt=="cattle"], ests$mo_jit[ests$trt=="cattle"], ests$rich_est[ests$trt=="cattle"]+ests$rich_SE[ests$trt=="cattle"],col="gray0",lwd=2,length=0.05, angle=90, code=3)
-
-legend("topright",legend=c("Bison","Cattle"), bty="n", pt.cex=2,cex=1.3, pch=c(21), pt.bg=c("sienna","gray0"),col=c("sienna","gray0"))
+legend("topright",legend=c("B"), bty="n", cex=2)
+##
 
 ##
 ests <- read.csv("Outputs/trtmo_ests_KS.csv")
@@ -45,7 +47,7 @@ head(ests)
 max(ests$abund_est)
 options(scipen = 999)
 
-plot(1, type="n", xlim=c(110,345), ylim=c(0.1,3200),las=1,ylab="",xlab="",log="y")
+plot(1, type="n", xlim=c(110,345), ylim=c(0.05,3200),las=1,ylab="",xlab="",log="y")
 box(lwd=2)
 title(ylab="Dung beetle abundance/ KS site", line=3.5, cex.lab=1.3)
 title(xlab="Day of year", line=2.5, cex.lab=1.3)
@@ -54,9 +56,13 @@ points(ests$abund_est[ests$trt=="bison"] ~ ests$doy[ests$trt=="bison"],pch=21,co
 points(ests$abund_est[ests$trt=="bison"] ~ ests$doy[ests$trt=="bison"], col="sienna",type="l",lwd=2)
 arrows(ests$doy[ests$trt=="bison"], ests$abund_est[ests$trt=="bison"]-ests$abund_SE[ests$trt=="bison"], ests$doy[ests$trt=="bison"], ests$abund_est[ests$trt=="bison"]+ests$abund_SE[ests$trt=="bison"],col="sienna",lwd=2,length=0.05, angle=90, code=3)
 ##cattle
-points(ests$abund_est[ests$trt=="cattle"] ~ ests$doy[ests$trt=="cattle"],pch=21,col="gray0",bg="gray0",cex=2)
+points(ests$abund_est[ests$trt=="cattle"] ~ ests$doy[ests$trt=="cattle"],pch=22,col="gray0",bg="gray0",cex=2)
 points(ests$abund_est[ests$trt=="cattle"] ~ ests$doy[ests$trt=="cattle"],col="gray0",type="l",lwd=2)
 arrows(ests$doy[ests$trt=="cattle"], ests$abund_est[ests$trt=="cattle"]-ests$abund_SE[ests$trt=="cattle"], ests$doy[ests$trt=="cattle"], ests$abund_est[ests$trt=="cattle"]+ests$abund_SE[ests$trt=="cattle"],col="gray0",lwd=2,length=0.05, angle=90, code=3)
+##
+legend("bottomleft",legend=c("Bison","Cattle"), bty="n", pt.cex=2,cex=1.3, pch=c(21,22), pt.bg=c("sienna","gray0"),col=c("sienna","gray0"))
+##
+legend("topright",legend=c("C"), bty="n", cex=2)
 ##
 
 max(ests$rich_est)
@@ -69,9 +75,11 @@ points(ests$rich_est[ests$trt=="bison"] ~ ests$doy[ests$trt=="bison"],pch=21,col
 points(ests$rich_est[ests$trt=="bison"] ~ ests$doy[ests$trt=="bison"],col="sienna",type="l",lwd=2)
 arrows(ests$doy[ests$trt=="bison"], ests$rich_est[ests$trt=="bison"]-ests$rich_SE[ests$trt=="bison"], ests$doy[ests$trt=="bison"], ests$rich_est[ests$trt=="bison"]+ests$rich_SE[ests$trt=="bison"],col="sienna",lwd=2,length=0.05, angle=90, code=3)
 ##cattle
-points(ests$rich_est[ests$trt=="cattle"] ~ ests$doy[ests$trt=="cattle"],pch=21,col="gray0",bg="gray0",cex=2)
+points(ests$rich_est[ests$trt=="cattle"] ~ ests$doy[ests$trt=="cattle"],pch=22,col="gray0",bg="gray0",cex=2)
 points(ests$rich_est[ests$trt=="cattle"] ~ ests$doy[ests$trt=="cattle"],col="gray0",type="l",lwd=2)
 arrows(ests$doy[ests$trt=="cattle"], ests$rich_est[ests$trt=="cattle"]-ests$rich_SE[ests$trt=="cattle"], ests$doy[ests$trt=="cattle"], ests$rich_est[ests$trt=="cattle"]+ests$rich_SE[ests$trt=="cattle"],col="gray0",lwd=2,length=0.05, angle=90, code=3)
+legend("topright",legend=c("D"), bty="n", cex=2)
+##
 
 dev.off()
 ######################################################
